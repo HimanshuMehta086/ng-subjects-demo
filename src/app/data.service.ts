1a78c8ca46dcf65f44739b02a19785e73766fea9
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-10 21:27:54 +0530

    done

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private stateSubject = new Subject<string>();
 
-  constructor() { }
+  //  Expose the observable to the clients
+  //  This is a read-only stream
+  state$ = this.stateSubject.asObservable();
+
+  constructor() {}
+
+  //  API for components
+  set message(value: string) {
+    if (value.length < 10) {
+      this.stateSubject.next(value);
+    } else {
+      console.log('API validation error');
+    }
+  }
 }
